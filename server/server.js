01a116c8a3dc788f65ab1ffd2296ac14df088f98
Heather Jackson <hmjackson917@gmail.com>
2021-08-02 09:22:56 -0400

    update server file to correctly reflect GraphQL connection

--- server/server.js
+++ server/server.js
@@ -1,9 +1,24 @@
+// Import dependencies
 const express = require('express');
-const mongoose = require("mongoose");
-const routes = require('./routes');
+const { ApolloServer } = require('apollo-server-express');
+const path = require('path');
+
+// Database, GraphQL, Utility imports
+const db = require('./config/connection');
+const { typeDefs, resolvers } = require('./schemas');
+const { authMiddleware } = require('./utils/auth');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+// GraphQL
+const server = new ApolloServer({
+  typeDefs,
+  resolvers,
+  context: authMiddleware,
+});
+
+server.applyMiddleware({ app });
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
@@ -13,18 +28,13 @@ if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join(__dirname, '../client/build')));
 }
 
-app.use(routes);
-
-mongoose.connect(
-  process.env.MONGODB_URI || 'mongodb://localhost/googlebooks',
-  {
-    useNewUrlParser: true,
-    useUnifiedTopology: true,
-    useCreateIndex: true,
-    useFindAndModify: false
-  }
-);
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, '../client/build/index.html'));
+});
 
 db.once('open', () => {
-  app.listen(PORT, () => console.log(`🌍 Now listening on localhost:${PORT}`));
+  app.listen(PORT, () => {
+    console.log(`🌍 Now listening on localhost:${PORT}`);
+    console.log(`Use GraphQL at http://localhost:${PORT}${server.graphqlPath}`);
+  })
 });
