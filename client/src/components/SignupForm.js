544ffe186a28300008fc19b8cb6312453b18effd
Heather Jackson <hmjackson917@gmail.com>
2021-08-03 11:14:17 -0400

    ADD_USER implemented to replace addUser from API

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,7 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
+import { useMutation } from "@apollo/client";
 
-import { createUser } from '../utils/API';
+import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
 
 const SignupForm = () => {
@@ -16,6 +17,8 @@ const SignupForm = () => {
     const { name, value } = event.target;
     setUserFormData({ ...userFormData, [name]: value });
   };
+
+  const [addUser] = useMutation(ADD_USER);
 
   const handleFormSubmit = async (event) => {
     event.preventDefault();
