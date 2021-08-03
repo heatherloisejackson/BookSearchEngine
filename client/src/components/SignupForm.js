b799f12cf84f40bd490599e2cf0c5e5920d748a5
Heather Jackson <hmjackson917@gmail.com>
2021-08-03 11:19:48 -0400

    signup form addUser implemented throughout function to furter replace API referances

--- client/src/components/SignupForm.js
+++ client/src/components/SignupForm.js
@@ -1,9 +1,8 @@
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-import { useMutation } from "@apollo/client";
 
-import { ADD_USER } from "../utils/mutations";
 import Auth from '../utils/auth';
+import { ADD_USER } from "../utils/mutations";
 
 const SignupForm = () => {
   // set initial form state
@@ -31,15 +30,8 @@ const SignupForm = () => {
     }
 
     try {
-      const response = await createUser(userFormData);
-
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
-      const { token, user } = await response.json();
-      console.log(user);
-      Auth.login(token);
+      const { data } = await addUser({ variables: { ...userFormData } });
+      Auth.login(data.addUser.token);
     } catch (err) {
       console.error(err);
       setShowAlert(true);
